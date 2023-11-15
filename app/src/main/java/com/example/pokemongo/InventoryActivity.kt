0b5d3427d997d99fdff2db3be7343cb267055d1a
FarmBera <101335222+FarmBera@users.noreply.github.com>
2023-11-15 11:09:39 +0900

    layout binding

--- app/src/main/java/com/example/pokemongo/InventoryActivity.kt
+++ app/src/main/java/com/example/pokemongo/InventoryActivity.kt
@@ -2,10 +2,12 @@ package com.example.pokemongo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pokemongo.databinding.ActivityInventoryBinding
 
 class InventoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_inventory)
+        val binding = ActivityInventoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/example/pokemongo/LibraryActivity.kt
+++ app/src/main/java/com/example/pokemongo/LibraryActivity.kt
@@ -2,10 +2,17 @@ package com.example.pokemongo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pokemongo.databinding.ActivityLibraryBinding
 
 class LibraryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_library)
+        val binding = ActivityLibraryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        // Event Process
     }
+
+
 }
